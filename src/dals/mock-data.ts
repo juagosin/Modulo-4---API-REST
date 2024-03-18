f2fcb914c8dd725909a7423535fa5ed4ff59fdc9
Juan <juagosin@gmail.com>
2024-03-18 20:40:12 +0100

    Fixed review model and mock-data

--- src/dals/mock-data.ts
+++ src/dals/mock-data.ts
@@ -3,14 +3,14 @@ import { Casa } from "./casa/index.js";
 import { Review } from "./review/index.js";
 
 export interface DB {
-  casas: Casa[];
-  reviews: Review[]
+  casas: Casa[];  
+  reviews: Review[];
 }
 
 export const db: DB = {
   casas: [
     {
-      _id: new ObjectId(),
+      _id: new ObjectId("65f894f64520ad878ef85eb9"),
       name: "Ribeira Charming Duplex",
       summary: "Fantastic duplex apartment with three bedrooms, located in the historic area of Porto, Ribeira (Cube) - UNESCO World Heritage Site. Centenary building fully rehabilitated, without losing their original character.",
       street: "Porto, Porto, Portugal",
@@ -22,7 +22,7 @@ export const db: DB = {
       }
     },
     {
-      _id: new ObjectId(),
+      _id: new ObjectId("65f895055a36ee682ce862b7"),
       name: "Cozy house at Beyoğlu",
       summary: "Hello dear Guests, wellcome to istanbul. My House is 2+1 and at second floor. 1 privite room is for my international guests. House is Very close to Taksim Square. You can Walk in 30 minutes or you can take a bus.  The bus stop is only 100 m from home. You can go Taksim, Eminönü, Karaköy, Kadıköy, Beyazıt, Sultanahmet easily from home.  I have 1 bed, two people can sleep together. Second person should pay extra. You can use kitchen, bathroom, free Wifi, dishwasher, washing machine, Ironing.",
       street: "Beyoğlu, İstanbul, Turkey", 
@@ -46,7 +46,7 @@ export const db: DB = {
       }
     },
     {
-      _id: new ObjectId(),
+      _id: new ObjectId("65f8950bec108ac9a8350fe1"),
       name: "Alugo Apart frente mar Barra Tijuca",
       summary: "Residencial com Serviços, Sala em 2 ambientes,varanda,quarto,cozinha,wc. Frente para o Mar da Barra, condomínio com piscina aquecida, sauna, hidromassagem,salões de festa,academia,cabeleireiro,play, mobiliado com todos os eletrodomésticos.",
       street: "Rio de Janeiro, Rio de Janeiro, Brazil",
@@ -58,7 +58,7 @@ export const db: DB = {
       }
     },
     {
-      _id: new ObjectId(),
+      _id: new ObjectId("65f895124ffd3816c2b2f710"),
       name: "Where Castles and Art meet the Sea",
       summary: "The flat is located in Matosinhos, part of Porto's District, where you can have the best of both worlds: the magic and beauty of Porto's Old City and the Beaches, Central Park and Seafood restaurants, with plenty of eclectic entertainment.",
       street: "Matosinhos, Porto, Portugal",
@@ -70,7 +70,7 @@ export const db: DB = {
       }
     },
     {
-      _id: new ObjectId(),
+      _id: new ObjectId("65f8951e2f1a0cbd12a8c68d"),
       name: "Charming Spacious Park Slope Studio",
       summary: "pacious and inviting garden level studio apartment in townhouse with separate entrance and large kitchen located in Park Slope, very near the F and G subway and Prospect Park, and only a few steps away from 7th Avenue, with its many fine shops and restaurants.",
       street: "Brooklyn, NY, United States",
@@ -82,7 +82,7 @@ export const db: DB = {
       }
     },
     {
-      _id: new ObjectId(),
+      _id: new ObjectId("65f8951934fd8bf966d9ce44"),
       name: "Asia House, Garden Room, Princevile",
       summary: "Asia House, sits on an ocean bluff in Princeville on the spectacular north shore of Kauai. The ocean or Queen's Bath is right across the street. There are beaches all over Princeville, all within 1-2 miles or less. Princeville Resort is a jewel on Hanalei Bay, known as one of the most beautiful bays and beaches in the world. This location is very quiet. Kauai has it all... spectacular scenery, gracious people, bird-watching, whale watching, snorkeling, diving, golfing, hiking, and more.",
       street: "Princeville, HI, United States",
@@ -93,5 +93,8 @@ export const db: DB = {
         fecha: new Date('1998-11-16T11:25'),
       }
     },
+  ],
+  reviews:[
+    
   ]
 };
--- src/dals/review/review.model.ts
+++ src/dals/review/review.model.ts
@@ -3,6 +3,5 @@ export interface Review {
     _id: ObjectId;
     autor: string;
     review: string;
-    fecha: Date;
-    casa_id: ObjectId;
+    fecha: Date;    
   }
